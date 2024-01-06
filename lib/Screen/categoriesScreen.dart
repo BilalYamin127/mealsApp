66193ad76c21b9565432058e837192857c5094e6
Mohamma BIlal <billi12786@gmail.com>
2024-01-06 14:09:45 +0500

    first commit

---
+++ lib/Screen/categoriesScreen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/Widget/Category-Grid-Items.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/Screen/mealsScreen.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // print(availableCategories);
+
+    void _selectcategory(BuildContext context,Category category) {
+    final filteredMeals=  dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) =>  MealsScreen(
+            title: category.title,
+            meals: filteredMeals,
+          ),
+        ),
+      ); // Navigator.push(context, route)
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick Your Category'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        children: [
+          // availableCategories.
+          // map((category) => CategoryGridItems(category: category)).toList(),
+          for (final category in availableCategories)
+            CategoryGridItems(
+              category: category,
+              OnselectedCategory: (){
+                _selectcategory(context, category);
+              }
+            )
+        ],
+      ),
+    );
+  }
+}
